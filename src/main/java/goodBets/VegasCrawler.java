62c614adde6cfacf82f0f6a28b059a6059da9130
Meredith Nachman <mwnachman@gmail.com>
2019-07-16 12:41:03 -0400

    grabbing date/time, teams, odds

--- src/main/java/goodBets/VegasCrawler.java
+++ src/main/java/goodBets/VegasCrawler.java
@@ -3,44 +3,51 @@ package goodBets;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class VegasCrawler {
 
 	public static void main(String[] args) {
 		String address = "http://www.vegasinsider.com/mlb/odds/las-vegas/?s=538";
-		URL pageLocation = null;
-		
+		Document doc = null;
 		try {
-			pageLocation = new URL(address);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		
-		Scanner in = null;
-		try {
-			in = new Scanner(pageLocation.openStream());
+			doc = Jsoup.connect(address).get();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		String htmlText = null;
-		while (in.hasNextLine()) {
-			htmlText += in.nextLine();
+		Elements oddsTable = doc.select("table.frodds-data-tbl");
+		for (Element element : oddsTable.select("tr")) {
+			List<Element> tds = element.children();
+			if (tds.size() > 4) {
+				Element teams = tds.get(0);
+				List<Element> children = teams.children();
+				if (children.size() > 4) {
+					Element dateTime = children.get(0);
+					System.out.println(dateTime.text());
+					Element awayTeam = children.get(2);
+					System.out.println(awayTeam.text());
+					Element homeTeam = children.get(4);
+					System.out.println(homeTeam.text());
+				}
+				Element odds = tds.get(2);
+				String[] oddsArray = odds.text().split(" ");
+				if (oddsArray.length > 2) {
+					String awayOdds = oddsArray[1];
+					System.out.println(awayOdds);
+					String homeOdds = oddsArray[2];
+					System.out.println(homeOdds);
+				}
+				System.out.println("");
+
+			}
 		}
-		
-		Document document = Jsoup.parse(htmlText);
-        Elements allElements = 
-            document.getAllElements();
-        for (Element element : allElements) {
-            System.out.println(element.nodeName() 
-            + " " + element.ownText());
-        }
 	}
 
 }
