8429f37793b0b3a2ffcdfc9d78529d2f9be72b80
“Eric <ericrice@seas.upenn.edu>
2019-07-28 12:08:03 -0400

    Fixed erros in Runner.java

--- src/main/java/goodBets/Runner.java
+++ src/main/java/goodBets/Runner.java
@@ -1,14 +1,15 @@
 package goodBets;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Runner {
 	
-	private ArrayList<Game> games;
-	private FiveThirtyEightReader fter = new FiveThirtyEightReader();
-	private double requiredEdge = 3.0; // tbd what this should be and whether it should be hardcoded, or the user shhould be prompted for an input
+	private static ArrayList<Game> games;
+	private static FiveThirtyEightReader fter = new FiveThirtyEightReader();
+	private static double requiredEdge = 3.0; // tbd what this should be and whether it should be hardcoded, or the user shhould be prompted for an input
 	
-	public ArrayList<Game> getGames() {
+	public static ArrayList<Game> getGames() {
 		VegasCrawler vc = new VegasCrawler();
 		ArrayList<Game> games = vc.crawlVegasSite();
 		return games;
@@ -20,7 +21,7 @@ public class Runner {
 		File out = new File("suggestions.txt");
 		try(PrintWriter pw = new PrintWriter(out) ) {
 			for(Game g : games) {
-				if g.largerDelta() => requiredEdge{
+				if(g.largerDelta() >= requiredEdge){
 					pw.println(g);
 				}
 			}
