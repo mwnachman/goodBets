52229b33a5d73ef284b26e571dfcb4123709da32
hedgehog770 <51975839+hedgehog770@users.noreply.github.com>
2019-07-27 21:11:22 -0400

    Update Runner.java
    Update runner to incorporate 538 Reader, iterate through the games arraylist, and print suggested games to a new file.

--- src/main/java/goodBets/Runner.java
+++ src/main/java/goodBets/Runner.java
@@ -3,11 +3,10 @@ package goodBets;
 import java.util.ArrayList;
 
 public class Runner {
-	private ArrayList<Game> games;
 	
-	public Runner() {
-		games = getGames();
-	}
+	private ArrayList<Game> games;
+	private FiveThirtyEightReader fter = new FiveThirtyEightReader();
+	private double requiredEdge = 3.0; // tbd what this should be and whether it should be hardcoded, or the user shhould be prompted for an input
 	
 	public ArrayList<Game> getGames() {
 		VegasCrawler vc = new VegasCrawler();
@@ -16,6 +15,19 @@ public class Runner {
 	}
 	
 	public static void main(String[] args) {
-		Runner r = new Runner();
+		games = getGames();
+		fter.updateGames(games);
+		File out = new File("suggestions.txt");
+		try(PrintWriter pw = new PrintWriter(out) ) {
+			for(Game g : games) {
+				if g.largerDelta() => requiredEdge{
+					pw.println(g);
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Could not write the File out.  Check permissions, or contact course staff for help");
+		}
 	}
 }
